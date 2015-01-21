5f8bb9abd5b450a5b4f405fae93b7847f491b47a
Oleg Korsunsky <jatruadmin@gmail.com>
2015-01-21 10:46:12 +0000

    Remove predefined highlight style

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   summary: "Code highlighting integrated with the markdown package",
   version: "1.0.9",
-  name: "simple:highlight.js",
-  git: "https://github.com/stubailo/meteor-highlight.js"
+  name: "wilddeer:highlight.js",
+  git: "https://github.com/wilddeer/meteor-highlight.js"
 });
 
 Npm.depends({
@@ -13,7 +13,6 @@ Package.onUse(function (api) {
   api.addFiles("detect-ie8.js");
   api.addFiles("highlight.pack.js");
   api.addFiles("markdown-integration.js");
-  api.addFiles("github.css");
   api.use("markdown@1.0.2", ["client", "server"], {weak: true});
   api.export("hljs");
 });
